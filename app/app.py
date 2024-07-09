1adac53b8de9ee9ed1a8740ebce796924303fd0d
liuchanggen <liuchanggen@noahgroup.com>
2024-07-09 11:08:09 +0800

    a

--- app/app.py
+++ app/app.py
@@ -2,8 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return 'Hello, World!'
+
 if __name__ == '__main__':
-         app.run (debug=True)
+    app.run(host='0.0.0.0', port=5000)
